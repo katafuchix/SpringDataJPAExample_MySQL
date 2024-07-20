5ac151c01af74d2d8bc64e3d3b7f16228c4bee6b
katafuchix <katafuchix@gmail.com>
2024-07-20 20:45:17 +0900

    HTML分割

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -2,8 +2,8 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.service.EmployeeServiceImpl;
 
@@ -15,21 +15,20 @@ public class EmployeeController {
     EmployeeServiceImpl employeeService;
     
 	@GetMapping("/")
-    public String index(Model model) {
+    /*public String index(Model model) {
         model.addAttribute("title", "コンテンツページ");
         System.out.print("aaa");       
         return "index";
-    }
-	/*
+    }*/
 	public ModelAndView listAll() {
-        
 		ModelAndView mav = new ModelAndView("index");
+
 		
 		// すべてのユーザーを取得する
 		mav.addObject("employees", employeeService.findAllEmployee());
 		return mav;
 	}
-	*/
+	
 	
 	/*
     @GetMapping("/")
