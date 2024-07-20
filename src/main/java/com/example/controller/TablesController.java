f2647727cf6e784a7324282ac228bce78a8e7ee9
katafuchix <katafuchix@gmail.com>
2024-07-21 01:37:05 +0900

    html

--- src/main/java/com/example/controller/TablesController.java
+++ src/main/java/com/example/controller/TablesController.java
@@ -2,37 +2,31 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.service.EmployeeServiceImpl;
 
 @Controller
-//@RestController
-public class EmployeeController {
-
+public class TablesController {
+	
     @Autowired
     EmployeeServiceImpl employeeService;
     
-	@GetMapping("/")
-    /*public String index(Model model) {
-        model.addAttribute("title", "コンテンツページ");
-        System.out.print("aaa");       
-        return "index";
-    }*/
+	@GetMapping("/tables")
 	public ModelAndView listAll() {
-		ModelAndView mav = new ModelAndView("index");
+		ModelAndView mav = new ModelAndView("tables");
 		
 		// すべてのユーザーを取得する
 		mav.addObject("employees", employeeService.findAllEmployee());
 		return mav;
 	}
 	
-	
-	/*
-    @GetMapping("/")
-    public ArrayList<Employee> getAllEmployee() {
-    	return employeeService.findAllEmployee();
+	@GetMapping("/table1")
+	public String index(Model model) {
+        model.addAttribute("title", "コンテンツページ");
+        System.out.print("aaa");       
+        return "table1";
     }
-    */
 }
